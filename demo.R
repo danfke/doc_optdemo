db510822a284487f84c2e4fd9d135aeed702ae56
Daniel King <dnlkng13@gmail.com>
2021-11-18 11:52:54 -0800

    Add new argument to demo.R

---
+++ demo.R
@@ -0,0 +1,16 @@
+# author: Daniel King edited from Tiffany Timbers code
+# date: 2021-11-18
+
+"This script prints out docopt args.
+Usage: demo.R <arg1> [<arg2>] --arg3=<arg2> [--arg4=<arg3>]
+Options:
+<arg>             Takes any value (this is a required positional argument)
+[<arg>]           Takes any value (this is an optional positional argument)
+--arg3=<arg3>     Takes any value (this is a required option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
+" -> doc
+library(docopt)
+opt <- docopt(doc)
+print(opt)
+print(typeof(opt))
+print(opt$arg2)
